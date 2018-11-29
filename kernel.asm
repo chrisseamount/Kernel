9172f5dbf28837e06f73ef157e1fd8e4fe0dc4a3
Anthony Kolendo <anthonykolendo@gmail.com>
2018-11-28 16:12:45 -0900

    adding keyboard support

--- kernel.asm
+++ kernel.asm
@@ -8,11 +8,35 @@ section .text ;;start of the command list
     dd - (0x1BADB002 + 0x00) ;; checksum that should set total between flag and magic nubmer back to zero.
 
 global start
-
+global keyboard_handler
+global read_port
+global write_port
+global load_idt
 
 extern kernalMain  ;; kernamlMain is our main function defined in the c file
+extern keyboard_handler_main
 
+read_port:
+	mov edx, [esp + 4]
+			;al is the lower 8 bits of eax
+	in al, dx	;dx is the lower 16 bits of edx
+	ret
 
+write_port:
+	mov   edx, [esp + 4]    
+	mov   al, [esp + 4 + 4]  
+	out   dx, al  
+	ret
+
+load_idt:
+	mov edx, [esp + 4]
+	lidt [edx]
+	sti 				;turn on interrupts
+	ret
+
+keyboard_handler:                 
+	call    keyboard_handler_main
+	iretd
 
 start:
     cli ;; clear all interrupts
--- kernel.c
+++ kernel.c
@@ -1,30 +1,131 @@
 // Created by Matthew Perry
 // Basic Kernal that makes Kernal name on page
+#include "keyboard_map.h"
+#define LINES 25
+#define COLUMNS_LINE 80
+#define BYTES_ELEMENT 2
+#define SCREENSIZE BYTES_ELEMENT * COLUMNS_LINE * LINES
+
+#define KEYBOARD_DATA_PORT 0x60
+#define KEYBOARD_STATUS_PORT 0x64
+#define IDT_SIZE 256
+#define INTERRUPT_GATE 0x8e
+#define KERNEL_CODE_SEGMENT_OFFSET 0x08
+
+#define ENTER_KEY_CODE 0x1C
+
+extern unsigned char keyboard_map[128];
+extern void keyboard_handler(void);
+extern char read_port(unsigned short port);
+extern void write_port(unsigned short port, unsigned char data);
+extern void load_idt(unsigned long *idt_ptr);
+
+
+
+const char *str = "This is the Kernal Loading up.";
 char *videoPtr = (char *) 0xb8000; //setting up video memory beginnning at 0xb8000
 unsigned int dWindow = 0; // loop count for drawing video on screen.
 unsigned int stringLocation = 0;  
 unsigned int cWindow = 0; // loop counter for clearing window/
 
-int stringLen(const char *str){
-        int i = 0;
-    while(str[i] != '\0'){
-        i++;
-    }    
-    if(i%2 == 1){
-       i++;
-    }
-return i;   
-}
-
-int centerLine(const char *str){
-    int x = stringLen(str);
-    return (80-x);
-}
-
+struct IDT_entry {
+    unsigned short int offset_lowerbits;
+    unsigned short int selector;
+    unsigned char zero;
+    unsigned char type_attr;
+    unsigned short int offset_higherbits;
+};
+struct IDT_entry IDT[IDT_SIZE];
 void sleep(){
     for(int i = 0; i < 2000000; i ++){
     }
     return;
+}void idt_init(void)
+{
+    unsigned long keyboard_address;
+    unsigned long idt_address;
+    unsigned long idt_ptr[2];
+
+    /* populate IDT entry of keyboard's interrupt */
+    keyboard_address = (unsigned long)keyboard_handler;
+    IDT[0x21].offset_lowerbits = keyboard_address & 0xffff;
+    IDT[0x21].selector = KERNEL_CODE_SEGMENT_OFFSET;
+    IDT[0x21].zero = 0;
+    IDT[0x21].type_attr = INTERRUPT_GATE;
+    IDT[0x21].offset_higherbits = (keyboard_address & 0xffff0000) >> 16;
+
+    /*     Ports
+    *    PIC1   PIC2
+    *Command 0x20   0xA0
+    *Data    0x21   0xA1
+    */
+
+    /* ICW1 - begin initialization */
+    write_port(0x20 , 0x11);
+    write_port(0xA0 , 0x11);
+
+    /* ICW2 - remap offset address of IDT */
+    /*
+    * In x86 protected mode, we have to remap the PICs beyond 0x20 because
+    * Intel have designated the first 32 interrupts as "reserved" for cpu exceptions
+    */
+    write_port(0x21 , 0x20);
+    write_port(0xA1 , 0x28);
+
+    /* ICW3 - setup cascading */
+    write_port(0x21 , 0x00);
+    write_port(0xA1 , 0x00);
+
+    /* ICW4 - environment info */
+    write_port(0x21 , 0x01);
+    write_port(0xA1 , 0x01);
+    /* Initialization finished */
+
+    /* mask interrupts */
+    write_port(0x21 , 0xff);
+    write_port(0xA1 , 0xff);
+
+    /* fill the IDT descriptor */
+    idt_address = (unsigned long)IDT ;
+    idt_ptr[0] = (sizeof (struct IDT_entry) * IDT_SIZE) + ((idt_address & 0xffff) << 16);
+    idt_ptr[1] = idt_address >> 16 ;
+
+    load_idt(idt_ptr);
+}
+void newLine(){
+    unsigned int lineSize = BYTES_ELEMENT*COLUMNS_LINE;
+    dWindow = dWindow + (lineSize - dWindow % (lineSize));
+}
+
+void kb_init(void)
+{
+    /* 0xFD is 11111101 - enables only IRQ1 (keyboard)*/
+    write_port(0x21 , 0xFD);
+}
+
+void keyboard_handler_main(void)
+{
+    unsigned char status;
+    char keycode;
+
+    /* write EOI */
+    write_port(0x20, 0x20);
+
+    status = read_port(KEYBOARD_STATUS_PORT);
+    /* Lowest bit of status will be set if buffer is not empty */
+    if (status & 0x01) {
+        keycode = read_port(KEYBOARD_DATA_PORT);
+        if(keycode < 0)
+            return;
+
+        if(keycode == ENTER_KEY_CODE) {
+            newLine();
+            return;
+        }
+
+        videoPtr[dWindow++] = keyboard_map[(unsigned char) keycode];
+        videoPtr[dWindow++] = 0x07;
+    }
 }
 
 void clear(){
@@ -40,7 +141,7 @@ void clear(){
     dWindow = 0;
     stringLocation = 0;
 }
-void draw(const char *str){
+void draw(){
   // writing string to video memory
     while(str[stringLocation] != '\0') {
         //setting char in str to char at dWindow in video memory
@@ -52,7 +153,7 @@ void draw(const char *str){
     }
     stringLocation = 0;
 }
-void drawSlow(const char *str){
+void drawSlow(){
     while(str[stringLocation] != '\0') {
         sleep();
         //setting char in str to char at dWindow in video memory
@@ -65,16 +166,13 @@ void drawSlow(const char *str){
     stringLocation = 0;
 }
 
-void newLine(){
-    unsigned int lineSize = 160;
-    dWindow = dWindow + (lineSize - dWindow % (lineSize));
-}
+
 
 void centerScreen(){
-    dWindow = 0;
     for(int i = 0; i < 12; i++){
         newLine();
     }   
+    
 }
 void blackLineTop(){
     cWindow = 158;
@@ -128,35 +226,34 @@ void drawBox(){
 }
 
 void kernalMain(){
-    unsigned int x = 0;
-    const char *str = "This is the Kernal Loading up..";
+ 
     clear();
     centerScreen();
-    x = centerLine(str);
-    dWindow += x;
-    drawSlow(str);
+    draw();
     str = ".........";
-    drawSlow(str);
+    drawSlow();
     clear();
     drawBox();
-    str = "Welcome to OS Lite";
     centerScreen();
-    dWindow += centerLine(str);
-    drawSlow(str);
+    dWindow += 62;
+    str = "Welcome to OS Lite";
+    drawSlow();
     str = "Home";
     newLine();
-    dWindow += centerLine(str);
-    draw(str);
+    dWindow += 76;
+    draw();
     str = "Help";
     newLine();
-    dWindow += centerLine(str); 
-    draw(str);
+    dWindow += 76;
+    draw();
     str = "Created by Matt Chris and Anthony";
     newLine();
     newLine();
     newLine();
-    dWindow += centerLine(str);
-    draw(str);
+    dWindow += 46;
+    draw();
+    idt_init();
+    kb_init();
     return;
     
 }

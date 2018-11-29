606d6ff0eab0c1a183ce9a2686ca580b2795c21a
mperry37 <mperry37@alaska.edu>
2018-11-28 15:24:44 -0900

    updated kernel file and build script

--- kernel.c
+++ kernel.c
@@ -1,10 +1,26 @@
 // Created by Matthew Perry
 // Basic Kernal that makes Kernal name on page
-const char *str = "This is the Kernal Loading up.";
 char *videoPtr = (char *) 0xb8000; //setting up video memory beginnning at 0xb8000
 unsigned int dWindow = 0; // loop count for drawing video on screen.
 unsigned int stringLocation = 0;  
 unsigned int cWindow = 0; // loop counter for clearing window/
+
+int stringLen(const char *str){
+        int i = 0;
+    while(str[i] != '\0'){
+        i++;
+    }    
+    if(i%2 == 1){
+       i++;
+    }
+return i;   
+}
+
+int centerLine(const char *str){
+    int x = stringLen(str);
+    return (80-x);
+}
+
 void sleep(){
     for(int i = 0; i < 2000000; i ++){
     }
@@ -24,7 +40,7 @@ void clear(){
     dWindow = 0;
     stringLocation = 0;
 }
-void draw(){
+void draw(const char *str){
   // writing string to video memory
     while(str[stringLocation] != '\0') {
         //setting char in str to char at dWindow in video memory
@@ -36,7 +52,7 @@ void draw(){
     }
     stringLocation = 0;
 }
-void drawSlow(){
+void drawSlow(const char *str){
     while(str[stringLocation] != '\0') {
         sleep();
         //setting char in str to char at dWindow in video memory
@@ -55,10 +71,10 @@ void newLine(){
 }
 
 void centerScreen(){
+    dWindow = 0;
     for(int i = 0; i < 12; i++){
         newLine();
     }   
-    
 }
 void blackLineTop(){
     cWindow = 158;
@@ -112,32 +128,35 @@ void drawBox(){
 }
 
 void kernalMain(){
- 
+    unsigned int x = 0;
+    const char *str = "This is the Kernal Loading up..";
     clear();
     centerScreen();
-    draw();
+    x = centerLine(str);
+    dWindow += x;
+    drawSlow(str);
     str = ".........";
-    drawSlow();
+    drawSlow(str);
     clear();
     drawBox();
-    centerScreen();
-    dWindow += 62;
     str = "Welcome to OS Lite";
-    drawSlow();
+    centerScreen();
+    dWindow += centerLine(str);
+    drawSlow(str);
     str = "Home";
     newLine();
-    dWindow += 76;
-    draw();
+    dWindow += centerLine(str);
+    draw(str);
     str = "Help";
     newLine();
-    dWindow += 76;
-    draw();
+    dWindow += centerLine(str); 
+    draw(str);
     str = "Created by Matt Chris and Anthony";
     newLine();
     newLine();
     newLine();
-    dWindow += 46;
-    draw();
+    dWindow += centerLine(str);
+    draw(str);
     return;
     
 }

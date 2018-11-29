6e993233a652b677c76348535c86bd4497334f05
chrisseamount <chris_seamount@hotmail.com>
2018-11-28 16:27:16 -0900

    fixing build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ~/Kernel
+cd ~/Documents/Kernel
 nasm -f elf32 kernel.asm -o kasm.o
 gcc -fno-stack-protector -m32 -c kernel.c -o kc.o
 ld -m elf_i386 -T link.ld -o kernel kasm.o kc.o

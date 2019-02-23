f2e75f059e227175c026159de4166f6a973dc812
chrisseamount <chris_seamount@hotmail.com>
2019-02-23 13:14:51 -0900

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ~/Documents/Kernel2/Kernel
-nasm -f elf32 kernel.asm -o kasm.o
-gcc -fno-stack-protector -m32 -c kernelFunctions.c -o kcf.o
-gcc -fno-stack-protector -m32 -c kernel.c -o kc.o
-ld -m elf_i386 -T link.ld -o kernel kasm.o kc.o kcf.o
-qemu-system-i386 -kernel kernel

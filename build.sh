6dbe559145aa12b1ac8856a9c5f3f46603a37979
chrisseamount <chris_Seamount@hotmail.com>
2019-04-24 18:55:28 -0800

    fixing build

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd ~/Documents/Kernel
+nasm -f elf32 kernel.asm -o kasm.o
+gcc -fno-stack-protector -m32 -c kernelFunctions.c -o kcf.o
+gcc -fno-stack-protector -m32 -c kernel.c -o kc.o
+gcc -fno-stack-protector -m32 -c keyboardFunctions.c -o kf.o
+gcc -fno-stack-protector -m32 -c chell.c -o ch.o
+gcc -fno-stack-protector -m32 -c DtoH.c -o dth.o
+gcc -fno-stack-protector -m32 -c DtoB.c -o dtb.o
+gcc -fno-stack-protector -m32 -c calc.c -o cal.o
+ld -m elf_i386 -T link.ld -o kernel kasm.o kc.o kcf.o kf.o ch.o dth.o dtb.o cal.o
+qemu-system-i386 -kernel kernel
